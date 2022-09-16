1b1ffbbb134e260be8c77f9793672156697cb6c5
LylaChapman-10 <821587@mystma.org>
2022-09-16 14:38:27 -0500

    3

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def  topFiveMovies(a, b, c, d, e):
-    return a + " " + b + " " + c  + " "  + d + " " + e + " "
+    return a + " " + b + " " + c  + " "  + d + " " + e 
 print(topFiveMovies("Ruby Sparks " , "Prisoner " , "The Batman " ,"Swiss Army Man ", "okja "))
